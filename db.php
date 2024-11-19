70252ed0ac6d510dde9c1bf8474119f8fe0f54a6
Nicole Espiritu <146156901+143nicolay@users.noreply.github.com>
2024-11-19 13:52:48 +0800

    db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'crud_app';
+$servername = "localhost";
+$username = "nicole";
+$password = "12345678";
+$database = "crud_app";
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
